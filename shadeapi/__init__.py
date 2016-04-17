5596a9e1742a587e76150b79b5b97a00681e1a0f
Monty Taylor <mordred@inaugust.com>
2016-04-16 21:11:03 -0500

    Add get_ resources

--- shadeapi/__init__.py
+++ shadeapi/__init__.py
@@ -1,38 +1,88 @@
-from flask import Flask
-from flask_restplus import Resource, Api
+# Copyright (c) 2016 Monty Taylor
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import flask
+import flask_restplus
 import shade
 
-app = Flask(__name__)
-api = Api(app)
+app = flask.Flask(__name__)
+api = flask_restplus.Api(app)
 all_clouds = {}
+CLOUD_API = shade.OpenStackCloud.__dict__.keys()
 
 
-class Config(Resource):
-    def get(self):
+def _get_cloud(cloud, region):
+    if cloud not in all_clouds:
+        all_clouds[cloud] = shade.openstack_cloud(
+            cloud=cloud, debug=True)
+    return all_clouds[cloud]
+
+
+class Config(flask_restplus.Resource):
+    def get(self, cloud='vexxhost', region=None):
         return cloud.cloud_config.config
+api.add_resource(
+    Config, '/config',
+    '/cloud/<string:cloud>/config',
+    '/cloud/<string:cloud>/region/<string:region>/config',
+    endpoint='config')
 
-def make_resource(name):
-    class RestResource(Resource):
+
+def make_list_resource(name):
+    class RestResource(flask_restplus.Resource):
         def get(self, cloud='vexxhost', region=None, **kwargs):
+            cloud_obj = _get_cloud(cloud, region)
+            return getattr(cloud_obj, name)()
+    return RestResource
+
+
+def make_get_resource(name):
+    class RestResource(flask_restplus.Resource):
+        def get(self, name_or_id, cloud='vexxhost', region=None, **kwargs):
             if cloud not in all_clouds:
                 all_clouds[cloud] = shade.openstack_cloud(
                     cloud=cloud, debug=True)
-            return getattr(all_clouds[cloud], name)()
+            return getattr(all_clouds[cloud], name)(name_or_id)
     return RestResource
 
-for f in shade.OpenStackCloud.__dict__.keys():
-    if not f.startswith('list_'):
+
+for list_key in CLOUD_API:
+    if not list_key.startswith('list_'):
         continue
-    name = f[5:]
-    res_class = make_resource(f)
+    name = list_key[5:]
+    single_name = name[:-1]
+    res_class = make_list_resource(list_key)
 
     api.add_resource(
         res_class, '/{res}'.format(res=name),
         '/cloud/<string:cloud>/{res}'.format(res=name),
         '/cloud/<string:cloud>/region/<string:region>/{res}'.format(res=name),
-        endpoint=f)
+        endpoint=list_key)
 
-api.add_resource(Config, '/config')
+    get_key = 'get_{name}'.format(name=single_name)
+
+    if get_key in CLOUD_API:
+        get_class = make_get_resource(get_key)
+        api.add_resource(
+            get_class,
+            '/{res}/<string:name_or_id>'.format(res=single_name),
+            '/cloud/<string:cloud>/{res}/<string:name_or_id>'.format(
+                res=single_name),
+            '/cloud/<string:cloud>/region/<string:region>/{res}'.format(
+                res=single_name),
+            endpoint=get_key)
+
 if __name__ == '__main__':
     app.run(debug=True)
